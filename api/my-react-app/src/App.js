24d16ab19239e59a4071d2e787ec42a432d0f9c2
DOREENKDAVID <doreenkinya153@gmail.com>
2024-02-22 23:14:46 +0300

    intergrated landing page to react app

--- api/my-react-app/src/App.js
+++ api/my-react-app/src/App.js
@@ -2,12 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import './Payments';
 import SignUpForm from './signup';
+import UserLogin from './LoginForm';
+import PasswordReset from './PasswordReset';
+import TransactionDetails from './Transactions'
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <SignUpForm />
+     <div className="App">
+      <Router>
+        <section>
+	  <Routes>
+	    <Route path="/signup" element={ <SignUpForm />} />
+	    <Route path="/login" element={ <UserLogin /> } />
+      <Route path='/reset-password' element={ <PasswordReset /> } />
+      <Route path='/account' element={ <TransactionDetails /> } />
+	    
+	  </Routes>
+	</section>
+      </Router>
     </div>
+
   );
 }
 
--- api/my-react-app/src/signup.js
+++ api/my-react-app/src/signup.js
@@ -1,58 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Style.css'
+import { NavLink, useNavigate } from 'react-router-dom';
 
-const SignUpForm = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
+const UserRegistrationForm = () => {
+    return (
+        <div className="container">
+            <h1>User Registration Form</h1>
+            <form action="/register" method="POST">
+                <div className="form-group row">
+                    <label htmlFor="firstname" className="col-sm-2 col-form-label">First Name:</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="firstname" name="firstname" placeholder='John'required />
+                    </div>
+                </div>
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
-  };
+                <div className="form-group row">
+                    <label htmlFor="lastname" className="col-sm-2 col-form-label">Last Name:</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="lastname" name="lastname" placeholder='Doe' required />
+                    </div>
+                </div>
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+                
+                <div className="form-group row">
+                    <label htmlFor="email" className="col-sm-2 col-form-label">Email:</label>
+                    <div className="col-sm-10">
+                        <input type="email" className="form-control" id="email" name="email" placeholder='johndoe@gmail.com' required />
+                    </div>
+                </div>
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+                <div className="form-group row">
+                    <label htmlFor="phonenumber" className="col-sm-2 col-form-label">Phone Number:</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="phonenumber" name="phonenumber" placeholder='+254 700000000'/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label htmlFor="password" className="col-sm-2 col-form-label">Password:</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="password" name="password" placeholder='enter your password' required />
+                    </div>
+                </div>
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // You can add your sign-up logic here, for example, send data to an API
-    console.log('Submitted:', { firstName, lastName, email, password });
-  };
+                <div className="form-group row">
+                    <label htmlFor="confirm password" className="col-sm-2 col-form-label">confirm password:</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="confirm password" name="confirm password" placeholder='confirm password'required />
+                    </div>
+                </div>
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input type="text" value={firstName} onChange={handleFirstNameChange} />
-      </label>
-      <br />
-      <label>
-        Last Name:
-        <input type="text" value={lastName} onChange={handleLastNameChange} />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <br />
-      <button type="submit">Sign Up</button>
-    </form>
-  );
+
+                <div className="form-group row">
+                    <div className="col-sm-10 offset-sm-2">
+                        <button type="submit" className="btn btn-primary">Register</button>
+                        <p>
+                        Do have an account?{' '}
+                        <NavLink to='/login'>
+                        login{' '}
+                        </NavLink>
+                        </p>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 };
 
-export default SignUpForm;
+export default UserRegistrationForm;
